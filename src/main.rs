be92f87051fc3be12c929229ca2db9d86770ab93
ImplFerris <implferris@proton.me>
2025-06-06 07:56:22 +0530

    panic handler function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 #![no_std]
 #![no_main]
 
-use panic_halt as _;
+// use panic_halt as _;
 
 use cortex_m_rt::entry;
+
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
 
 #[entry]
 fn main() -> ! {
